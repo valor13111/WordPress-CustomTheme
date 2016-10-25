85b83a4919539d0ca9c8908c66d1bc21b70ddac6
Tyler Reed <valor13111@gmail.com>
2016-10-25 02:54:26 -0400

    Added author date, number of posts by author, and thumbnail image

--- search.php
+++ search.php
@@ -13,11 +13,17 @@
       echo "<h2> Searched Results: " . get_search_query() . " </h2>";
 
       while(have_posts()) : the_post();
+        /* gets the thumbnail for the post and displays it */
+        echo get_the_post_thumbnail(get_the_ID(), medium) . '</br>';
+        /* gets the title and anchors it */
         echo '<a href="' . get_the_permalink() .'">' . get_the_title() . '</a><br/>';
+        /* gets the author/published date for the post */
+        echo "Published by " . get_the_author() . "(" . get_the_author_posts() . ")<br/>";
         echo "Published on " . get_the_date() . "<br/>";
+        /* only allows the first 100 characters to be displayed from the post */
         $content = substr(strip_tags(get_the_content()), 0, 100) . '...';
 
-        # Read More link
+        /* Read More link */
         echo $content;
         echo '<a href="' . get_the_permalink() . '">Read More</a><br/>';
         echo '<hr/>';
