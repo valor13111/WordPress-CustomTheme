f82de0afd2b1edf636dbc8622643e3223a7d9304
Tyler Reed <valor13111@gmail.com>
2016-10-27 01:45:02 -0400

    Added category.php for category templates, instead of them referring to index.php

---
+++ category.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+<body>
+  <h1><?php echo bloginfo('title'); ?></h1>
+  <div class="section group">
+	<div class="col span_1_of_3">
+	This is column 1
+	</div>
+	<div class="col span_1_of_3">
+
+    <h1>Category: <?php echo single_cat_title(); ?></h1>
+	   <?php
+      while ( have_posts() ) : the_post(); ?>
+        <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+          <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+
+        <div class="entry">
+          <?php the_content(); ?>
+
+          <p class="postmetadata">
+            <?php
+              comments_popup_link('No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed');
+            ?>
+          </p>
+        </div>
+    <?php endwhile; ?>
+	</div>
+	<div class="col span_1_of_3">
+    <?php get_sidebar(); ?>
+	</div>
+
+  <?php get_footer(); ?>
+</div>
+</body>
+</html>
