137569dd6a3e1b5189452683cf6b35c7dfa40446
Tyler Reed <valor13111@gmail.com>
2017-01-12 21:16:26 -0500

    Added footer.php to hook into any page.

--- footer.php
+++ footer.php
@@ -1,3 +1,20 @@
 <?php wp_footer(); ?>
 
-<footer>&copy; <?php echo the_current_year(); ?> Designed By Tyler Reed</footer>
+
+<footer>
+
+<?php
+  wp_nav_menu(
+    array(
+      'theme_location'    => 'footer_menu',
+      'fallback_cb'       => false,
+      'menu_id'           => 'footer-menu',
+      'menu_class'        => 'footer-menus',
+      'after'             => ' | '
+    )
+  );
+?>
+
+  &copy; <?php echo the_current_year(); ?> Designed By Tyler Reed
+
+</footer>
